708a5666d013df0869a078f1004a74c13a19563e
Admin <Admin@TEB127030>
2023-03-10 08:58:41 +0100

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace uppgift4_7
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv något");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Det kommer skrivas ut vertikalt, hur många steg åt höger vill du ha det?");
+            int steg = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
